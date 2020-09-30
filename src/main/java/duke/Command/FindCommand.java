b6e6f3933cb0c6fc22107c2401a44a3cdfd77f27
liewws <liewws98@gmail.com>
2020-09-30 15:29:38 +0800

    Revert "Revert "Handle case where item cannot be found in list""
    This reverts commit fa89aad380fdb262159d986cd39617a06cc12418.

--- src/main/java/duke/Command/FindCommand.java
+++ src/main/java/duke/Command/FindCommand.java
@@ -7,13 +7,29 @@ import duke.Ui;
 
 import java.io.IOException;
 
+/**
+ * Represents the command to find tasks in the list.
+ */
 public class FindCommand extends Command {
     private String key;
 
+    /**
+     * Constructor for the FindCommand object.
+     * @param key String to be searched for in the names of the tasks.
+     */
     public FindCommand(String key) {
         this.key = key;
         commandType = CommandType.CMD_FIND;
     }
+
+    /**
+     * Execute the command to find the key in the tasks.
+     * @param dukeList DukeList to be searched.
+     * @param ui Ui object to display search results in console.
+     * @param store Not used here.
+     * @throws IOException Not used here.
+     * @throws DukeException if the list is empty or the item is not in the list.
+     */
     @Override
     public void executeCommand(DukeList dukeList, Ui ui, Storage store) throws IOException, DukeException {
         ui.printOut(dukeList.findTasks(key));
